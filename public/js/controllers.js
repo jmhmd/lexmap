ba085cd44ceaf55ffb78fbabf28e1bd6d8e23ce8
Jason <jhostetter@gmail.com>
2013-08-27 18:16:27 -0400

    new hiliter working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,7 +4,7 @@
 
 angular.module('myApp.controllers', [])
 	.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
-		$scope.textToAnnotate = 'Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye'
+		$scope.textToAnnotate = 'Melanoma is a uterine artery embolization malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye'
 		$scope.annotationResult = ''
 
 		$scope.getAnnotations = function(){
@@ -14,9 +14,7 @@ angular.module('myApp.controllers', [])
 					var coords = {},
 						resultBox = $('#annotationResult'),
 						resultDetails = $('#resultDetails'),
-						text = $scope.textToAnnotate,
-						textLength = text.length,
-						newLength = textLength
+						text = $scope.textToAnnotate
 
 					console.log('request result', result)
 
@@ -37,34 +35,86 @@ angular.module('myApp.controllers', [])
 						}
 					})
 
-					termsSorted = _.sortBy(terms, function(term){ return term.from })
+					/*
+					/* start sorting out words, and which phrases they are included in
+					*/
 
-					console.log('terms', terms)
-					
-					// place spans around each matched term
-					_.forEach(termsSorted, function(term, i){
-						/*
-						/* New tactic
-						*/
+					// return first non-space character index
+					function getNextStart(text, startIndex){
+						var startIndex = startIndex || 0
 
-						// annotator incorrectly defines first character as position 1
-						term.from = term.from - 1
+						if (text.substr(startIndex, 1) != ' '){ return startIndex }
 
-						var lengthDiff = newLength - textLength,
-							openTag = '<span id="term_'+i+'">',
-							closeTag = '</span>',
-							beforeTerm = text.slice(0,term.from + lengthDiff),
-							termText = text.slice(term.from + lengthDiff, term.to + lengthDiff),
-							afterTerm = text.slice(term.to + lengthDiff),
-							replacedText = beforeTerm + openTag + termText + closeTag + afterTerm
+						var	nextSpace = text.indexOf(' ', startIndex),
+							nextWord = nextSpace + 1
 
-						text = replacedText
-						newLength = text.length
+						while (text.substr(nextWord, 1) == ' '){
+							nextWord += 1
+						}
+
+						return nextWord
+					}
+
+					// return first space character index
+					function getNextEnd(text, startIndex){
+						var end = text.indexOf(' ', startIndex || 0)
+						return end === -1 ? text.length : end
+					}
+
+					function getWords(text, startIndex, endIndex){
+						var startIndex = startIndex || 0,
+							endIndex = endIndex || text.length,
+							words = []
+
+						while (startIndex < endIndex) {
+							var next = getNextStart(text, startIndex),
+								end = getNextEnd(text, next)
+							words.push([next, end])
+							startIndex = end
+						}
+
+						return words
+					}
+
+					var newText = '',
+						words = getWords(text)
+
+					var i = 1
+					terms = _.map(terms, function(term){
+						term._id = i
+						i++
+						return term
 					})
 
-					resultBox.html(text)
+					var phrases = []
+					_.forEach(words, function(val, i){
+						phrases[i] = []
+						var classes = '',
+							word = text.substr(val[0],val[1]-val[0])
 
-					_.forEach(termsSorted, function(term, i){
+						_.forEach(terms, function(term){
+							if (val[0] === term.from - 1 || (val[0] > term.from - 1 && val[0] < term.to)){
+								phrases[i].push(term._id)
+								classes += 't_'+term._id+' '
+							}
+						})
+
+
+						if (phrases[i].length > 0){
+							newText += '<span id="w_'+i+'" class="'+classes+'">' + word + '</span> '
+
+						} else {
+							newText += word + ' '
+						}
+					})
+
+					console.log(newText)
+					console.log(phrases)
+					
+					resultBox.html(newText)
+
+					// now loop through terms/phrases and apply styles/actions
+					_.forEach(terms, function(term){
 						var termDetails = '<h4>Preferred Name:</h4>'+
 							'<span>'+term.term+'</span>'+
 							'<h4>Is a:</h4>'+
@@ -72,15 +122,17 @@ angular.module('myApp.controllers', [])
 							'<h4>Link:</h4>'+
 							'<a href="'+term.link+'">'+term.link+'</span>'
 
-						$('#term_'+i)
+						$('.t_'+term._id)
 							.attr({
 								'class': 'term_hilite'
 							})
+							//.css()
 							.data('details', termDetails)
 							.on('click', function(e){
 								resultDetails.html($(e.target).data('details'))
 							})
 					})
+
 				})
 		}
 }])
