6e20407a0fc75e17f936bc348b30bc6eba679dbe
Jason <jhostetter@gmail.com>
2013-08-28 19:53:03 -0400

    tabs functioning, just need to add file upload functionality

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,8 @@ angular.module('myApp', [
   'myApp.controllers',
   'myApp.filters',
   'myApp.services',
-  'myApp.directives'
+  'myApp.directives',
+  'ui.bootstrap'
 ]).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider.
