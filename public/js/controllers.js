50b905628a50ffcb71a043da0f7ce8b29bc401ca
Jason <jhostetter@gmail.com>
2013-08-30 13:50:42 -0400

    back to normal default text

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,8 +6,7 @@ angular.module('myApp.controllers', [])
 	.controller('AppCtrl', ['$scope', '$http', 'Annotator', function ($scope, $http, Annotator) {
 		var spinner = new Spinner({top: '50px'})
 
-		$scope.textToAnnotate = 'Melanoma is a uterine artery embolization malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye Melanoma is a uterine artery embolization malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye'
-		$scope.annotationResult = ''
+		$scope.textToAnnotate = 'Melanoma is a uterine artery embolization malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye'
 		$scope.uploadFile
 		$scope.options = {
 			showAllInstances: true
